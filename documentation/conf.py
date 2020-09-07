e56edcbf73a0cb112ced6c3f05c98a19f259a224
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2020-09-07 11:11:05 +0200

    Adds logo to documentation theme

--- documentation/conf.py
+++ documentation/conf.py
@@ -51,7 +51,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/images/IDSimF_logo_dark.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+#html_css_files = [
+#    'css/custom.css',
+#]
