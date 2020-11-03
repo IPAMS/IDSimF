f9a2dbe37809a899400893adbfb40cc0d2c7802c
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2020-11-03 22:15:54 +0100

    Adds configuration for running doxygen on read the docs servers

--- documentation/conf.py
+++ documentation/conf.py
@@ -76,3 +76,35 @@ html_static_path = ['_static']
 # Breathe Configuration
 breathe_projects = {"IDSimF":"_doxygen/xml"}
 breathe_default_project = "IDSimF"
+
+
+# Run doxygen for breathe on the read the docs servers: 
+# taken from https://breathe.readthedocs.io/en/latest/readthedocs.html 
+
+import subprocess, sys
+
+def run_doxygen(folder):
+    """Run the doxygen make command in the designated folder"""
+
+    try:
+        retcode = subprocess.call("cd %s; doxygen Doxyfile" % folder, shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+    except OSError as e:
+        sys.stderr.write("doxygen execution failed: %s" % e)
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands if we're on the ReadTheDocs server"""
+
+    read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+    if read_the_docs_build:
+
+        run_doxygen(".")
+
+
+def setup(app):
+
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
