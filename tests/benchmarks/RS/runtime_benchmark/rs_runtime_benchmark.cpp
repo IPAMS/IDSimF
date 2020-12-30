5f181996fe0ad124b9d1ed861033f272a31539ac
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2020-12-30 12:58:16 +0100

    Adds a simple runtime benchmark for a RS simulation

---
+++ tests/benchmarks/RS/runtime_benchmark/rs_runtime_benchmark.cpp
@@ -0,0 +1,99 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2020 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+
+ ------------
+
+ A simple runtime benchmarks for RS for benchmarking and profiling
+
+ ***************************/
+
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include "RS_Simulation.hpp"
+#include "RS_SimulationConfiguration.hpp"
+#include "RS_ConfigFileParser.hpp"
+#include "RS_ConcentrationFileWriter.hpp"
+
+int main(int argc, const char * argv[]) {
+
+    //read and prepare chemical configuration ===============================================
+    std::string confFileName = "RS_waterCl_static.conf";
+    RS::ConfigFileParser parser = RS::ConfigFileParser();
+    RS::Simulation rsSim = RS::Simulation(parser.parseFile(confFileName));
+    RS::SimulationConfiguration* simConf = rsSim.simulationConfiguration();
+
+    // init simulation  =====================================================================
+
+    // create and add simulation particles:
+    int nParticles = 10000;
+    std::vector<uniqueReactivePartPtr>particles;
+    std::vector<BTree::Particle*>particlesPtrs;
+    //std::vector<std::vector<double>> trajectoryAdditionalParams;
+
+    RS::Substance *subst = simConf->substance(0);
+    for (int i=0; i<nParticles;i++) {
+
+        uniqueReactivePartPtr particle = std::make_unique<RS::ReactiveParticle>(subst);
+        particle->setLocation({0.01*i, 0.0, 0.0});
+        particlesPtrs.push_back(particle.get());
+        rsSim.addParticle(particle.get(), i);
+        particles.push_back(std::move(particle));
+
+    }
+
+    RS::ReactionConditions reactionConditions = RS::ReactionConditions();
+    reactionConditions.temperature = 300;
+    reactionConditions.pressure = 100000;
+    reactionConditions.electricField = 0.0;
+    reactionConditions.totalReactionEnergy = 0.0;
+    // ======================================================================================
+
+
+    // simulate   ===========================================================================
+    int nSteps = 10000;
+    int concentrationPrintInterval = 100;
+    double dt_s = 5e-11;
+
+    clock_t begin = std::clock();
+
+    for (int step=0; step<nSteps; step++) {
+
+        for (int i = 0; i < nParticles; i++) {
+            bool reacted = rsSim.react(i, reactionConditions, dt_s);
+        }
+        rsSim.advanceTimestep(dt_s);
+
+        if (step % concentrationPrintInterval ==0) {
+            rsSim.printConcentrations();
+        }
+
+    }
+
+    clock_t end = std::clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "reaction events:" << rsSim.totalReactionEvents() << " ill events:" << rsSim.illEvents() << std::endl;
+    std::cout << "ill fraction: " << rsSim.illEvents() / (double) rsSim.totalReactionEvents() << std::endl;
+    std::cout << "elapsed seconds "<< elapsed_secs<<std::endl;
+
+    // ======================================================================================
+
+    return 0;
+}
