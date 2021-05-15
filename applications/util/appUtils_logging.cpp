cd5a0f9e320c2829906cba4ba877094007d43691
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2021-05-16 00:10:14 +0200

    Add basic logging facility and tests of logging

---
+++ applications/util/appUtils_logging.cpp
@@ -0,0 +1,42 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2021 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+ ****************************/
+#include "appUtils_logging.hpp"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+
+std::shared_ptr<spdlog::logger> AppUtils::createLogger(std::string logfileName) {
+
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::info);
+    console_sink->set_pattern("[%^%l%$] %v");
+
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfileName, true);
+    file_sink->set_level(spdlog::level::info);
+    file_sink->set_pattern("[%Y-%m-%d %T][%L] %v");
+
+    std::vector<spdlog::sink_ptr> sinks = {console_sink, file_sink};
+    auto logger = std::make_shared<spdlog::logger>("logger", begin(sinks), end(sinks));
+    logger->set_level(spdlog::level::info);
+    spdlog::register_logger(logger);
+
+    return logger;
+}
---
+++ tests/applications/test_logging.cpp
@@ -0,0 +1,43 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2021 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+
+ ------------
+ test_logging.cpp
+
+ Tests of logging facilities
+
+ ****************************/
+
+#include "catch.hpp"
+#include "appUtils_logging.hpp"
+#include "appUtils_simulationConfiguration.hpp"
+#include "test_util.hpp"
+#include <string>
+
+TEST_CASE( "Test logging", "[ApplicationUtils]") {
+    auto logger = AppUtils::createLogger("test.log");
+    logger->info("First log message");
+    logger->info("Second log message");
+    logger->flush();
+
+    // test written contents:
+    std::string lfContents = readTextFile("test.log");
+    CHECK(lfContents.find("[I] Second log message") != std::string::npos);
+}
