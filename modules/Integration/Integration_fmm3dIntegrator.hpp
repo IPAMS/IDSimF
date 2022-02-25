5830330d718f5677c945c4edcf1cf6305290aabd
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2022-02-25 03:29:35 +0100

    Adds source files for FMM3D Integrator

---
+++ modules/Integration/Integration_fmm3dIntegrator.hpp
@@ -0,0 +1,32 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2022 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+
+ ------------
+ Integration_fmm3dIntegrator.hpp
+
+ Fast multipole method (FMM) based ion trajectory integration
+ (simple verlet trajectory integrator) including space charge
+
+ ****************************/
+
+#ifndef IDSIMF_INTEGRATION_FMM3DINTEGRATOR_HPP
+#define IDSIMF_INTEGRATION_FMM3DINTEGRATOR_HPP
+
+#endif //IDSIMF_INTEGRATION_FMM3DINTEGRATOR_HPP
