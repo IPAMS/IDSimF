d467a535e3b2c59de30e881e78fa53026abe5d57
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2022-02-26 02:31:37 +0100

    Adds skeleton of a FMM solver based on FMM3D library

---
+++ modules/SpaceCharge/FMM3D/FMM3D_fmmSolver.hpp
@@ -0,0 +1,46 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2022 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+ ------------
+
+FMM3D_fmmSolver.hpp
+
+ Coulombic Particle / Particle (space charge) solver based on FMM3D library
+
+ ****************************/
+
+#ifndef IDSIMF_FMM3D_FMMSOLVER_HPP
+#define IDSIMF_FMM3D_FMMSOLVER_HPP
+
+#include "SC_generic.hpp"
+#include "Core_vector.hpp"
+#include "Core_particle.hpp"
+
+namespace FMM3D{
+
+    /**
+     * Abstract base class for Barnes-Hut Tree nodes
+     */
+    class FMMSolver : public SpaceCharge::FieldCalculator {
+
+    public:
+        [[nodiscard]] Core::Vector computeEFieldFromSpaceCharge(Core::Particle& particle) override;
+    };
+}
+#endif //IDSIMF_FMM3D_FMMSOLVER_HPP
---
+++ tests/modules/SpaceCharge/FMM3D/test_fmmSolver.cpp
@@ -0,0 +1,40 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2020 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+
+ ------------
+ test_node.cpp
+
+ Testing of nodes of the serial version of the Barnes-Hut tree
+
+ ****************************/
+#include "FMM3D_fmmSolver.hpp"
+#include "Core_vector.hpp"
+#include "Core_particle.hpp"
+#include "catch.hpp"
+#include "test_util.hpp"
+
+TEST_CASE( "Test basic particle/particle interaction calculation with FMM3D", "[FMM3D]"){
+    FMM3D::FMMSolver fmmSolver;
+
+    Core::Particle testParticle;
+    Core::Vector spaceChargeForce = fmmSolver.computeEFieldFromSpaceCharge(testParticle);
+
+    CHECK(spaceChargeForce.x() == 1.0);
+}
