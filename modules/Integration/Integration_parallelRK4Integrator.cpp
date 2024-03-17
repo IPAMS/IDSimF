f4f91146ff9e3253cb6a5464adbf915fd06867e1
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2024-03-18 00:51:58 +0100

    Adds skeleton of parallel Runge Kutta 4 integrator

---
+++ modules/Integration/Integration_parallelRK4Integrator.cpp
@@ -0,0 +1,177 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2020 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+ ****************************/
+
+#include "Integration_parallelRK4Integrator.hpp"
+#include <utility>
+#include <algorithm>
+
+Integration::ParallelRK4Integrator::ParallelRK4Integrator(
+        const std::vector<Core::Particle *>& particles,
+        Integration::accelerationFctType accelerationFunction,
+        Integration::timestepWriteFctType timestepWriteFunction,
+        Integration::otherActionsFctType otherActionsFunction,
+        Integration::AbstractTimeIntegrator::particleStartMonitoringFctType ionStartMonitoringFunction,
+        CollisionModel::AbstractCollisionModel* collisionModel) :
+    AbstractTimeIntegrator(particles, ionStartMonitoringFunction),
+    collisionModel_(collisionModel),
+    accelerationFunction_(std::move(accelerationFunction)),
+    timestepWriteFunction_(std::move(timestepWriteFunction)),
+    otherActionsFunction_(std::move(otherActionsFunction))
+{}
+
+Integration::ParallelRK4Integrator::ParallelRK4Integrator(
+        Integration::accelerationFctType accelerationFunction,
+        Integration::timestepWriteFctType timestepWriteFunction,
+        Integration::otherActionsFctType otherActionsFunction,
+        Integration::AbstractTimeIntegrator::particleStartMonitoringFctType ionStartMonitoringFunction,
+        CollisionModel::AbstractCollisionModel* collisionModel) :
+    AbstractTimeIntegrator(ionStartMonitoringFunction),
+    collisionModel_(collisionModel),
+    accelerationFunction_(std::move(accelerationFunction)),
+    timestepWriteFunction_(std::move(timestepWriteFunction)),
+    otherActionsFunction_(std::move(otherActionsFunction))
+{
+    initInternalState_();
+}
+
+
+/**
+ * Adds a particle to the verlet integrator (required if particles are generated in the course of the simulation
+ * @param particle the particle to add to the verlet integration
+ */
+void Integration::ParallelRK4Integrator::addParticle(Core::Particle *particle){
+    particles_.push_back(particle);
+    newPos_.emplace_back(Core::Vector(0,0,0));
+    a_t_.emplace_back(Core::Vector(0,0,0));
+    a_tdt_.emplace_back(Core::Vector(0,0,0));
+
+    tree_.insertParticle(*particle, nParticles_);
+    ++nParticles_;
+}
+
+void Integration::ParallelRK4Integrator::bearParticles_(double time) {
+    Integration::AbstractTimeIntegrator::bearParticles_(time);
+    initInternalState_();
+}
+
+void Integration::ParallelRK4Integrator::initInternalState_(){
+    tree_.init();
+}
+
+void Integration::ParallelRK4Integrator::run(unsigned int nTimesteps, double dt) {
+
+    // run init:
+    this->runState_ = RUNNING;
+    bearParticles_(0.0);
+
+    if (timestepWriteFunction_ !=nullptr) {
+        timestepWriteFunction_(particles_, time_, timestep_, false);
+    }
+
+    // run:
+    for (unsigned int step=0; step< nTimesteps; step++){
+        runSingleStep(dt);
+        if (this->runState_ == IN_TERMINATION){
+            break;
+        }
+    }
+    this->finalizeSimulation();
+    this->runState_ = STOPPED;
+}
+
+void Integration::ParallelRK4Integrator::runSingleStep(double dt){
+
+    //first: Generate new particles if necessary
+    bearParticles_(time_);
+
+    if (collisionModel_ !=nullptr){
+        collisionModel_->updateModelTimestepParameters(timestep_, time_);
+    }
+    std::size_t i;
+    #pragma omp parallel \
+            default(none) shared(newPos_, a_tdt_, a_t_, dt, particles_) \
+            private(i) //firstprivate(MyNod)
+    {
+
+        #pragma omp for schedule(dynamic, 40)
+        for (i=0; i<nParticles_; i++){
+
+            if (particles_[i]->isActive()){
+
+                if (collisionModel_ != nullptr) {
+                    collisionModel_->updateModelParticleParameters(*(particles_[i]));
+                }
+
+                newPos_[i] = particles_[i]->getLocation() + particles_[i]->getVelocity() * dt + a_t_[i]*(1.0/2.0*dt*dt);
+                a_tdt_[i] = accelerationFunction_(particles_[i], i, tree_, time_, timestep_);
+                //acceleration changes due to background interaction:
+
+                if (collisionModel_ != nullptr) {
+                    collisionModel_->modifyAcceleration(a_tdt_[i], *(particles_[i]), dt);
+                }
+
+                particles_[i]->setVelocity( particles_[i]->getVelocity() + ((a_t_[i]+ a_tdt_[i])*1.0/2.0 *dt) );
+                a_t_[i] = a_tdt_[i];
+
+                //velocity changes due to background interaction:
+                if (collisionModel_ != nullptr) {
+                    //std::cout << "before:" << particles_[i]->getVelocity() << std::endl;
+                    collisionModel_->modifyVelocity(*(particles_[i]),dt);
+                    //std::cout << "after:" << particles_[i]->getVelocity() << std::endl;
+                }
+            }
+        }
+    }
+
+    // First find all new positions, then perform otherActions then update tree.
+    // This ensures that all new particle positions are found with the state from
+    // last time step. No particle positions are found with a partly updated tree.
+    // Additionally, the update step is not (yet) parallel.
+    for (std::size_t i=0; i<nParticles_; i++){
+        if (particles_[i]->isActive()){
+            //position changes due to background interaction:
+            if (collisionModel_ != nullptr) {
+                collisionModel_->modifyPosition(newPos_[i], *(particles_[i]), dt);
+            }
+
+            if (otherActionsFunction_ != nullptr) {
+                otherActionsFunction_(newPos_[i], particles_[i], i, time_, timestep_);
+            }
+        }
+    }
+
+    time_ = time_ + dt;
+    timestep_++;
+    if (timestepWriteFunction_ != nullptr) {
+        timestepWriteFunction_(particles_, time_, timestep_, false);
+    }
+}
+
+/**
+ * Finalizes the RK4 integration run (should be called after the last time step).
+ */
+void Integration::ParallelRK4Integrator::finalizeSimulation(){
+    if (timestepWriteFunction_ != nullptr){
+        timestepWriteFunction_(particles_, time_, timestep_, true);
+    }
+}
+
+
---
+++ modules/Integration/Integration_parallelRK4Integrator.hpp
@@ -0,0 +1,93 @@
+/***************************
+ Ion Dynamics Simulation Framework (IDSimF)
+
+ Copyright 2020 - Physical and Theoretical Chemistry /
+ Institute of Pure and Applied Mass Spectrometry
+ of the University of Wuppertal, Germany
+
+ IDSimF is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ IDSimF is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with IDSimF.  If not, see <https://www.gnu.org/licenses/>.
+
+ ------------
+ BTree_parallelRK4Integrator.hpp
+ 
+ Parallel version of a Runge Kutta 4 Integrator
+ Note: Currently, no space charge is considered here
+
+ ****************************/
+
+#ifndef BTree_parallelRK4Integrator_hpp
+#define BTree_parallelRK4Integrator_hpp
+
+#include "Core_particle.hpp"
+#include "Core_vector.hpp"
+#include "BTree_parallelTree.hpp"
+#include "Integration_abstractTimeIntegrator.hpp"
+#include "CollisionModel_AbstractCollisionModel.hpp"
+#include <vector>
+
+namespace Integration{
+
+    //std::function<Core::Vector(Core::Particle* particle, int particleIndex, Core::Tree& tree, double time, int timestep)> accelerationFctType;
+
+    class ParallelRK4Integrator: public AbstractTimeIntegrator {
+
+        public:
+
+            ParallelRK4Integrator(
+                    const std::vector<Core::Particle*>& particles,
+                    accelerationFctType accelerationFunction,
+                    timestepWriteFctType timestepWriteFunction = nullptr,
+                    otherActionsFctType otherActionsFunction = nullptr,
+                    AbstractTimeIntegrator::particleStartMonitoringFctType ionStartMonitoringFunction = nullptr,
+                    CollisionModel::AbstractCollisionModel* collisionModel = nullptr
+            );
+
+            ParallelRK4Integrator(
+                    accelerationFctType accelerationFunction,
+                    timestepWriteFctType timestepWriteFunction = nullptr,
+                    otherActionsFctType otherActionsFunction = nullptr,
+                    AbstractTimeIntegrator::particleStartMonitoringFctType ionStartMonitoringFunction = nullptr,
+                    CollisionModel::AbstractCollisionModel* collisionModel = nullptr
+            );
+
+            void addParticle(Core::Particle* particle) override;
+            void run(unsigned int nTimesteps, double dt) override;
+            void runSingleStep(double dt) override;
+            void finalizeSimulation() override;
+
+    private:
+
+        CollisionModel::AbstractCollisionModel* collisionModel_ = nullptr; ///< the gas collision model to perform while integrating
+
+        accelerationFctType accelerationFunction_ = nullptr;   ///< function to calculate particle acceleration
+        timestepWriteFctType timestepWriteFunction_ = nullptr; ///< function to export / write time step results
+        otherActionsFctType otherActionsFunction_ = nullptr;   ///< function for arbitrary other actions in the simulation
+
+        //internal variables for actual calculations:
+        Core::Vector loc_min_ = Core::Vector(-1000,-1000,-1000); ///< currently hard coded lower corner of the sim. domain
+        Core::Vector loc_max_ = Core::Vector( 1000, 1000, 1000); ///< currently hard coded upper corner of the sim. domain
+        BTree::ParallelTree tree_ = BTree::ParallelTree(loc_min_,loc_max_);
+        ///< The parallel BTree (primarily for space charge calculation)
+
+        std::vector<Core::Vector>  newPos_;  ///< new position (after time step) for particles
+        std::vector<Core::Vector>  a_t_;     ///< last time step acceleration for particles
+        std::vector<Core::Vector>  a_tdt_;   ///< new acceleration for particles
+
+        void bearParticles_(double time);
+        void initInternalState_();
+    };
+}
+
+
+#endif /* BTree_parallelRK4Integrator_hpp */
