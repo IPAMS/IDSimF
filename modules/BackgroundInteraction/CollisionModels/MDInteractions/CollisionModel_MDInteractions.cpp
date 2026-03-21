b7efdbe02268742113813ba8548b506279701680
Walter Wissdorf <wissdorf@uni-wuppertal.de>
2026-03-21 02:34:22 +0100

    Limits maximum number of time steps and adds missing time step length reduction factor

--- modules/BackgroundInteraction/CollisionModels/MDInteractions/CollisionModel_MDInteractions.cpp
+++ modules/BackgroundInteraction/CollisionModels/MDInteractions/CollisionModel_MDInteractions.cpp
@@ -370,7 +370,7 @@ void CollisionModel::MDInteractionsModel::modifyVelocity(Core::Particle& particl
         double finalTime = integrationTime_; //  final integration time in seconds
         double timeStep = subTimeStep_; // step size in seconds
 
-        trajectorySuccess = rk4InternAdaptiveStep(moleculesPtr, timeStep, finalTime, 1e10, collisionRadius, tolerance);
+        trajectorySuccess = rk4InternAdaptiveStep(moleculesPtr, timeStep, finalTime, 100000, collisionRadius, tolerance);
         //trajectorySuccess = leapfrogIntern(moleculesPtr, timeStep, finalTime, collisionRadius);
         double kineticEnergyEnd = 0, rotationEnergyEnd = 0;
         double endEnergy = 0;
@@ -394,6 +394,7 @@ void CollisionModel::MDInteractionsModel::modifyVelocity(Core::Particle& particl
                 std::cout << "Kinetic Energy: " << kineticEnergyStart << " " << kineticEnergyEnd << std::endl;
                 std::cout << "Rotation Energy: " << rotationEnergyStart << " " << rotationEnergyEnd << std::endl;
                 trajectorySuccess = false;
+                dt = dt/2;
             }
         }
 
